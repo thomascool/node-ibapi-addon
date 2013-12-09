930c679a70e2c089f3b49a154e31d51d6752d3bd
unknown <jaey226@gmail.com>
2013-12-09 14:45:29 -0800

    Relegated ibproc.js as barebones script

--- examples/ibproc.js
+++ examples/ibproc.js
@@ -1,47 +1,46 @@
+// This is a bare-bones setting for the scripts using node-ibapi-addon
 var addon = require('../build/Release/NodeIbapiAddon');
-var ibcontract = require('../lib/contract');
 var obj = new addon.NodeIbapi();
 
 obj.connect('127.0.0.1',7496,0);
 
 var orderId = -1;
 var counter = 0;
-var trackedOrder = [];
 var once = false;
+
+var clientError;
+var srvError;
 
 while(obj.isConnected()) {
   obj.checkMessages();
   obj.processMsg();
+  clientError = obj.getWinError();
+  srvError = obj.getError();
 
   if (!once && counter == 50000) {
     validOrderId = obj.getNextOrderId();
     orderId = validOrderId;
     console.log('Next valid order Id: %d',validOrderId);
     once = true;
+    counter = 0;
+  }
+  if (clientError[0] != "") {
+    console.log('Client error');
+  }
+  if (srvError[0] != -1) {
+    console.log('Error: ' + srvError[0].toString() + ' - ' + 
+      srvError[1].toString() + ' - ' + srvError[2].toString());
   }
   
+  // implement your functionalities here
   if (counter == 20000) {
     obj.reqIds(1);
   }
-  if (counter == 100000 && orderId > -1) {
+  if (counter == 50000) {
     validOrderId = obj.getNextOrderId();
+    orderId = validOrderId;
     console.log('Next valid order Id: %d',validOrderId);
-    console.log('Next order Id: %d',orderId);
-    console.log("Placing order for MSFT");
-    var newContract = ibcontract.createContract();
-    newContract.symbol = 'MSFT';
-    newContract.secType = 'STK';
-    newContract.exchange = 'SMART';
-    newContract.primaryExchange = 'NASDAQ';
-    newContract.currency = 'USD';
-    obj.placeOrder(orderId,newContract, 
-      "BUY", 1000, "LMT", 0.11);
-    orderId = orderId + 1;
-  }
-  if (counter == 200000 && orderId > -1) {
     counter = 0;
   }
-  
   counter = counter + 1;
-
 }
