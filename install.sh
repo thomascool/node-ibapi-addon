ab42e5157db15a3c800d23929c6eafed36a4dbd2
dchem <jaey226@gmail.com>
2014-01-08 22:21:09 -0500

    fixes file path

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ mkdir import
 pushd import
 wget http://interactivebrokers.github.io/downloads/twsapi_unixmac_969.02.jar
 jar xf twsapi_unixmac_969.02.jar
-cp ./IBJts/source/PosixClient/Shared/* .
+cp ./IBJts/source/PosixClient/shared/* .
 cp ./IBJts/source/PosixClient/src/* .
 sed -i '4i#define IB_USE_STD_STRING' EClientSocketBase.cpp
 sed -i '4i#define IB_USE_STD_STRING' EPosixClientSocket.cpp
