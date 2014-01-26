7938001bd89d7821ad0bd7583a28c9b70e33d51d
dchem <jaey226@gmail.com>
2014-01-25 22:10:30 -0500

    Fixes install.sh, makes sure that close() in EPosixClientSocketPlatform.h is compiled correctly

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
-mkdir import
-pushd import
+DIR="./import"
+PDIR="../"
+
+if [ ! -d "$DIR" ]
+then
+    mkdir $DIR
+fi
+cd $DIR
 wget http://interactivebrokers.github.io/downloads/twsapi_unixmac_969.02.jar
 jar xf twsapi_unixmac_969.02.jar
 cp ./IBJts/source/PosixClient/shared/* .
 cp ./IBJts/source/PosixClient/src/* .
 sed -i '4i#define IB_USE_STD_STRING' EClientSocketBase.cpp
 sed -i '4i#define IB_USE_STD_STRING' EPosixClientSocket.cpp
-popd
+sed -i 's_// LINUX_#include <unistd.h>_g' EPosixClientSocketPlatform.h
+cd $PDIR
 node-gyp rebuild
