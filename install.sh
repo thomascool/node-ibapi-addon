ca5e83260878e3ce85ac4bb2bc6ec73d706877a0
dchem <jaey226@gmail.com>
2014-03-10 19:03:23 -0400

    -_- Mac OS X

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ wget http://interactivebrokers.github.io/downloads/twsapi_unixmac_969.02.jar
 jar xf twsapi_unixmac_969.02.jar
 cp ./IBJts/source/PosixClient/shared/* .
 cp ./IBJts/source/PosixClient/src/* .
-sed -i "" -e '4i#define IB_USE_STD_STRING' EClientSocketBase.cpp
-sed -i "" -e '4i#define IB_USE_STD_STRING' EPosixClientSocket.cpp
-sed -i "" -e 's_// LINUX_#include <unistd.h>_g' EPosixClientSocketPlatform.h
+sed -ie '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
+sed -ie '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
+sed -ie 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
 cd $PDIR
 node-gyp rebuild
