794ba438bb09d5b0031a27ab53687398fde9004c
dchem <jaey226@gmail.com>
2014-03-17 17:35:16 -0400

    Changes unzippig command, determines which OS and uses proper SED command

--- preinstall.sh
+++ preinstall.sh
@@ -7,10 +7,21 @@ then
 fi
 cd $DIR
 wget http://interactivebrokers.github.io/downloads/twsapi_unixmac_969.02.jar
-jar xf twsapi_unixmac_969.02.jar
+unzip twsapi_unixmac_969.02.jar
 cp ./IBJts/source/PosixClient/shared/* .
 cp ./IBJts/source/PosixClient/src/* .
-sed -ie '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
-sed -ie '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
+
+if [ "$(uname)" == "Darwin" ]; then
+sed -ie $'4i\\\n#define IB_USE_STD_STRING\n' ./EClientSocketBase.cpp
+sed -ie $'4i\\\n#define IB_USE_STD_STRING\n' ./EPosixClientSocket.cpp
 sed -ie 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+sed -i '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
+sed -i '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
+sed -i 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+sed -i '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
+sed -i '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
+sed -i 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
+fi
 cd $PDIR
