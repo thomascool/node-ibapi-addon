19fa1b4f53847a1245e1d9864fb61beb8a8b479c
dchem <jaey226@gmail.com>
2014-03-20 04:26:06 -0400

    Removed bash'ism

--- preinstall.sh
+++ preinstall.sh
@@ -11,15 +11,15 @@ unzip twsapi_unixmac_969.02.jar
 cp ./IBJts/source/PosixClient/shared/* .
 cp ./IBJts/source/PosixClient/src/* .
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
 sed -ie $'4i\\\n#define IB_USE_STD_STRING\n' ./EClientSocketBase.cpp
 sed -ie $'4i\\\n#define IB_USE_STD_STRING\n' ./EPosixClientSocket.cpp
 sed -ie 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
 sed -i '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
 sed -i '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
 sed -i 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
 sed -i '4i#define IB_USE_STD_STRING' ./EClientSocketBase.cpp
 sed -i '4i#define IB_USE_STD_STRING' ./EPosixClientSocket.cpp
 sed -i 's_// LINUX_#include <unistd.h>_g' ./EPosixClientSocketPlatform.h
